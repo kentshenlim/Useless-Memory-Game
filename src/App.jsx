3ec197bb6df8c63b01367be125e072f2fd77350e
kentshenlim <kentshen_lim24@hotmail.com>
2023-08-14 14:14:55 +0100

    Can change difficulty now

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Ask from './components/Ask';
 import Loading from './components/Loading';
 import Game from './components/Game';
 import Fireflies from './components/Fireflies';
+import { useState } from 'react';
 
 function App() {
+  console.log('main');
+  const [difficulty, setDifficulty] = useState('medium');
+
   return (
     <div className="app-wrapper">
       <Fireflies number={5} />
-      <Ask />
+      <Ask difficulty={difficulty} setDifficulty={setDifficulty} />
       {/* <Loading /> */}
       {/* <Game /> */}
     </div>
--- src/components/Ask.jsx
+++ src/components/Ask.jsx
@@ -3,9 +3,14 @@ import a from '../assets/img/a.gif';
 import c from '../assets/img/c.gif';
 import e from '../assets/img/e.gif';
 import t from '../assets/img/t.gif';
+import PropTypes from 'prop-types';
 import './Ask.css';
 
-export default function Ask() {
+export default function Ask({ difficulty, setDifficulty }) {
+  function handleChangeDifficulty(e) {
+    setDifficulty(e.target.value);
+  }
+
   const unownImg = [a, c, e, t];
   const [aNode, cNode, eNode, tNode] = unownImg.map((imgSrc, idx) => (
     <Unown imgSrc={imgSrc} key={idx} /> // Will not change array during runtime, so accept idx as key
@@ -38,24 +43,45 @@ export default function Ask() {
       </div>
       <div className="ask-difficulty-wrapper">
         <div className="radio-wrapper">
-          <input type="radio" id="easy" name="difficulty" value="easy" />
+          <input
+            type="radio"
+            id="easy"
+            name="difficulty"
+            value="easy"
+            checked={difficulty === 'easy'}
+            onChange={handleChangeDifficulty}
+          />
           <label htmlFor="easy">Easy</label>
         </div>
         <div className="radio-wrapper">
-          <input type="radio" id="medium" name="difficulty" value="medium" />
+          <input
+            type="radio"
+            id="medium"
+            name="difficulty"
+            value="medium"
+            checked={difficulty === 'medium'}
+            onChange={handleChangeDifficulty}
+          />
           <label htmlFor="medium">Medium</label>
         </div>
         <div className="radio-wrapper">
-          <input type="radio" id="hard" name="difficulty" value="hard" />
+          <input
+            type="radio"
+            id="hard"
+            name="difficulty"
+            value="hard"
+            checked={difficulty === 'hard'}
+            onChange={handleChangeDifficulty}
+          />
           <label htmlFor="hard">Hard</label>
         </div>
       </div>
       <div className="ask-infos-wrapper">
-        <button>
+        <button type="button">
           <ion-icon name="play-circle-outline"></ion-icon>
           <p>Start</p>
         </button>
-        <button>
+        <button type="button">
           <ion-icon name="logo-github"></ion-icon>
           <p>GitHub</p>
         </button>
@@ -63,3 +89,8 @@ export default function Ask() {
     </main>
   );
 }
+
+Ask.propTypes = {
+  difficulty: PropTypes.string.isRequired,
+  setDifficulty: PropTypes.func.isRequired,
+};
